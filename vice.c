6a9a8aa7764ec3a9b7eac5ad5bdb50ad03543c18
georgekahl <george.kahl@icloud.com>
2026-07-23 13:07:38 -0500

    Hashkeys

--- vice.c
+++ vice.c
@@ -1,19 +1,10 @@
 #include "stdio.h"
+#include "stdlib.h" 
 #include "defs.h"
 int main(){
     AllInit();
-    int index = 0;
-    U64 playBitBoard = 0ULL;
-
-    for(index = 0; index < 64; index++){
-        printf("Index:%d\n", index);
-        PrintBitBoard(ClearMask[index]);
-        printf("\n");
-    }
     
-    /*
-    SETBIT(playBitBoard, 61);
-    PrintBitBoard(playBitBoard);
-    */
+    
+
     return 0;
 }
